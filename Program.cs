335f93b72c8afc25219cc64262b7d277b18b8a46
Jonathan Barreira Da Silva Nunes <jbnunes@sfiec.org.br>
2019-11-05 17:46:20 -0300

    ajuste program.cs

--- Program.cs
+++ Program.cs
@@ -14,7 +14,10 @@ namespace testeMVC
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001")
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
